c14452a7e3fedcd7a68ddf2d55ace42d04590251
Ndima-karabo17 <mhangwanikarabo@gmail.com>
2025-07-23 23:58:05 +0200

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,7 @@
+def greet(first_name, last_name):
+    print(f"Hi {first_name} {last_name}")
+
+first_name = input("What is your name? ")
+last_name = input("What is your surname? ")
+
+greet(first_name, last_name)
